c218993ef40464a24586375f1bd1b8302fe49c28
crgett <crgett@gmail.com>
2018-02-08 22:11:39 -0600

    switch to typewriterJS
    switched to typewriterJS and (hopefully) fixed the navbar flicker on
    click

--- resources/js/portfolio.js
+++ resources/js/portfolio.js
@@ -1,14 +1,30 @@
-var messages = ["an aspiring software engineer.",
-"an athlete.", "a renaissance man.", "a student.", "an explorer.",
-"a problem solver."];
+var obj = $('#type-effect')[0]; 
 
-var typed = new Typed("#typed", {
-	strings: messages,
-	typeSpeed: 80,
-    backSpeed: 40,
-    smartBackspace: true,
-    loop: true
+var typewriter = new Typewriter(obj, {
+	loop: true,
+	typingSpeed: 60,
+	deleteSpeed: 50
 });
+
+typewriter.typeString('an aspiring software engineer.')
+    .pauseFor(2500)
+    .deleteChars(27)
+    .typeString('athlete.')
+    .pauseFor(2500)
+    .deleteChars(10)
+    .typeString(' renaissance man.')
+    .pauseFor(2500)
+    .deleteChars(17)
+    .typeString(' student.')
+    .pauseFor(2500)
+    .deleteChars(9)
+    .typeString('n explorer.')
+    .pauseFor(2500)
+    .deleteChars(11)
+    .typeString(' problem solver.')
+    .pauseFor(2500)
+    .deleteAll()
+    .start();
 
 $(document).ready(function(){
     var scroll_start = 0;
@@ -17,9 +33,9 @@ $(document).ready(function(){
     $(document).scroll(function() {
         scroll_start = $(this).scrollTop();
         if(scroll_start > offset.top) {
-            $('#navbar').css('background-color', 'black');
+            $('#navbar').css('background-color', '#414141');
         } else {
-            $('#navbar').css('background-color', 'transparent');
+            $('#navbar').css('background-color', 'rgba(0, 0, 0, 0.5)');
         }
     });
 });
